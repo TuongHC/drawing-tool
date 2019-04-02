ef9a7c804adec9edf204f4b6856ab48456ac27f4
TuongHuynh <cattuongit@gmail.com>
2019-04-02 22:22:42 +0700

    remove comment user

--- src/lib/drawingTool.js
+++ src/lib/drawingTool.js
@@ -1,7 +1,6 @@
 // import dependencies
 // import { concat } from '../util/stringHandle';
 import DrawingCore from './drawingCore';
-// return UserList class
 export default class DrawingTool extends DrawingCore{
 
     constructor(elem, users){  
